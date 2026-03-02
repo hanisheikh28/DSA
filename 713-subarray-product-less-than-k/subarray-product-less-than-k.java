8128b0a5998dcbbe051a315a4d79dc1d6279e7a0
Ummehani M D <ummehani.281201@gmail.com>
2026-03-02 14:25:15 +0530

    Time: 4 ms (99.84%) | Memory: 51.3 MB (58.39%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.java
@@ -1,23 +1,17 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        if (k <= 1) return 0;              // products are positive integers, nothing < 1
-        int left = 0;
-        int count = 0;
-        long product = 1;                  // use long to reduce overflow risk
+        if(k<=1) return 0;
+     int left =0,prod = 1,count = 0;
+     for(int right=0;right<nums.length;right++){
+        prod *= nums[right];
 
-        for (int right = 0; right < nums.length; right++) {
-            product *= nums[right];
+        while(prod >= k && left <= right){
+            prod /= nums[left];
+            left++;
 
-            while (product >= k && left <= right) {
-                product /= nums[left];
-                left++;
-            }
-
-            // All subarrays ending at 'right' and starting from any index in [left..right]
-            // have product < k. That's (right - left + 1) many subarrays.
-            count += (right - left + 1);
         }
-
-        return count;
+        count+=(right-left+1);
+     }   
+     return count;
     }
 }
